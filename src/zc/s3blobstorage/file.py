7532e32fa4b375d6059da6cb789e352a67694ce9
Jim Fulton <jim@zope.com>
2013-10-01 13:40:12 -0400

    Added ZConfig support.
    Also renamed pack_blob to pack_blobs.

--- src/zc/s3blobstorage/file.py
+++ src/zc/s3blobstorage/file.py
@@ -1,11 +1,12 @@
 import os
 import time
 import zc.s3blobstorage.flat
+import ZODB.config
 import ZODB.FileStorage
 
 class FileStorage(ZODB.FileStorage.FileStorage):
 
-    def __init__(self, file_name, blob_dir, pack_blob=True, **kw):
+    def __init__(self, file_name, blob_dir, pack_blobs=True, **kw):
         ZODB.FileStorage.FileStorage.__init__(
             self, file_name, blob_dir=blob_dir, **kw)
 
@@ -14,7 +15,7 @@ class FileStorage(ZODB.FileStorage.FileStorage):
             result = original_packer(storage, referencesf, stop, gc)
             removed = os.path.join(storage.blob_dir, '.removed')
             if os.path.exists(removed):
-                if pack_blob:
+                if pack_blobs:
                     os.rename(
                         removed,
                         os.path.join(storage.blob_dir,
@@ -27,3 +28,27 @@ class FileStorage(ZODB.FileStorage.FileStorage):
 
     def _blob_init(self, blob_dir):
         ZODB.FileStorage.FileStorage._blob_init(self, blob_dir, 'flat')
+
+class Config(ZODB.config.BaseConfig):
+
+    def open(self):
+        config = self.config
+        options = {}
+        if getattr(config, 'packer', None):
+            packer = config.packer
+            if ':' in packer:
+                m, expr = packer.split(':', 1)
+                m = __import__(m, {}, {}, ['*'])
+                options['packer'] = eval(expr, m.__dict__)
+            else:
+                m, name = config.packer.rsplit('.', 1)
+                m = __import__(m, {}, {}, ['*'])
+                options['packer'] = getattr(m, name)
+
+        for name in ('blob_dir', 'create', 'read_only', 'quota', 'pack_gc',
+                     'pack_keep_old', 'pack_blobs'):
+            v = getattr(config, name, self)
+            if v is not self:
+                options[name] = v
+
+        return FileStorage(config.path, **options)
